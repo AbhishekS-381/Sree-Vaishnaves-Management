fd24e0a0bde8166e67991295e39d66762c7d5650
Abhishek S <abhishek31023@gmail.com>
2022-12-25 23:18:47 +0530

    Planning Done

--- setup/database_setup.sql
+++ setup/database_setup.sql
@@ -1,10 +1,8 @@
-CREATE DATABASE IF NOT EXISTS MinutesofMeet;
+CREATE DATABASE IF NOT EXISTS VaishnavesDB;
 
-CREATE TABLE IF NOT EXISTS user_details (employeeID INT PRIMARY KEY, employeeName VARCHAR(255), userToken VARCHAR(255), employeePassword VARCHAR(255), emailID VARCHAR(255), userType VARCHAR(255));
-CREATE TABLE IF NOT EXISTS team_details (teamName VARCHAR(255) PRIMARY KEY, details TEXT, managerName VARCHAR(255), managerID INT);
-CREATE TABLE IF NOT EXISTS team_mapping (employeeID INT, teamName VARCHAR(255));
-CREATE TABLE IF NOT EXISTS action_items (task VARCHAR(255), assignedTo INT, teamName VARCHAR(255), currentStatus INT DEFAULT 0, assignedDate DATETIME);
-CREATE TABLE IF NOT EXISTS meet_summary (teamName VARCHAR(255), startTime DATETIME, scrumMaster VARCHAR(255),content TEXT, endTime DATETIME);
-CREATE TABLE IF NOT EXISTS query_details (employeeID INT, employeeName VARCHAR(255), teamName VARCHAR(255), managerName VARCHAR(255), meetDate DATETIME, querySubject VARCHAR(255), details TEXT);
-CREATE TABLE IF NOT EXISTS minute_link (teamName VARCHAR(255), content VARCHAR(255), link VARCHAR(255), meetDate DATETIME);
+CREATE TABLE IF NOT EXISTS user_details (employeeID INT PRIMARY KEY, employeeName VARCHAR(255), userToken VARCHAR(255), employeePassword VARCHAR(255), userType VARCHAR(255), current_status INT);
+CREATE TABLE IF NOT EXISTS employee_details (employeeID INT PRIMARY KEY, employeeName VARCHAR(255), userToken VARCHAR(255), mobile INT, _address TEXT, salary INT, empType VARCHAR(255), joined DATETIME);
+CREATE TABLE IF NOT EXISTS stock_details (stockName VARCHAR(255), available INT, unit VARCHAR(255), stockType VARCHAR(255));
+CREATE TABLE IF NOT EXISTS stock_history (stockName VARCHAR(255), actionMade VARCHAR(255), change INT, unit VARCHAR(255), usedBy VARCHAR(255), changesDoneOn DATETIME);
+CREATE TABLE IF NOT EXISTS feedback_details (customerName VARCHAR(255), mobile INT, feedbackSubject VARCHAR(255), details TEXT, queryDate DATETIME, feedbackStatus INT DEFAULT 0);
 CREATE TABLE IF NOT EXISTS session_check (userToken VARCHAR(255), currentStatus INT);
